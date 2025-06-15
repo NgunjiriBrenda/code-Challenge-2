68073f38de706f35421c4f08ae870dda6c22ac84
brenda njoki <ngunjiribrenda5@gmail.com>
2025-06-15 18:40:13 +0300

    complete code challenge 2

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById('guest-form').addEventListener('submit',(event) =>{
+    event.preventDefault();
+    const name = document.getElementById('guest-name').value ();
+    const category = docume
+}
+)
