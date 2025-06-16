9473ae2a47a6b7920f4858967b1afb0b5c6abf70
brenda njoki <ngunjiribrenda5@gmail.com>
2025-06-16 13:14:44 +0300

    Adds styled guest list

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-
 const form = document.getElementById('guest-form');
 const list = document.getElementById('guest-list');
-
-form.addEventListener('submit', (e) =>{
+ 
+form.addEventListener('submit', e =>{
     e.preventDefault();
-    const name = document.getElementById('guest-name').value.trim();
-    const category = document.getElementById('guest-category').value ;
+    const name = document.getElementById('guest-name').value .trim();
+    const category = document.getElementById('guest-category').value;
+
     if(name && category){
         addGuest(name, category);
-        form.reset();
+        form.reset ();
     }
 });
 
 function addGuest(name, category){
     const guest = document.createElement('div');
     guest.innerHTML = `
-       <span>${name} - ${category}</span>
-       <button class = "delete">Remove</button>
-       `;
+    <span>${name} (${category}) </span>
+    <button class = "delete">Delete</button>`;
 
 
+    guest.classList.add(category.toLowerCase());
 
-    guest.querySelector('.delete').addEventListener('click' , () =>{
-        guest.remove();
-    });
+guest.querySelector('.delete').addEventListener('click', () =>{
+    guest.remove();
+
+});
     list.prepend(guest);
-    form.reset();
+
+}
